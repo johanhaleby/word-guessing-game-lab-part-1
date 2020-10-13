9214ce07101d3bf3457bda04593e1f71b3afa621
Johan Haleby <johan.haleby@gmail.com>
2020-10-13 13:00:38 +0200

    Using Sequence instead of Stream in AS

--- src/main/kotlin/se/parkster/occurrent/lab/wordguessinggame/application/ApplicationService.kt
+++ src/main/kotlin/se/parkster/occurrent/lab/wordguessinggame/application/ApplicationService.kt
@@ -4,21 +4,22 @@ import io.cloudevents.CloudEvent
 import org.occurrent.eventstore.api.blocking.EventStore
 import org.occurrent.eventstore.api.blocking.EventStream
 import se.parkster.occurrent.lab.wordguessinggame.domain.DomainEvent
-import java.util.stream.Stream
+import kotlin.streams.asSequence
+import kotlin.streams.asStream
 
 class ApplicationService constructor(private val eventStore: EventStore,
                                      private val cloudEventConverter: CloudEventConverter) {
 
-    fun execute(streamId: String, functionThatCallsDomainModel: (Stream<DomainEvent>) -> Stream<DomainEvent>) {
+    fun execute(streamId: String, functionThatCallsDomainModel: (Sequence<DomainEvent>) -> Sequence<DomainEvent>) {
         // Read all events from the event store for a particular stream
         val eventStream: EventStream<CloudEvent> = eventStore.read(streamId)
         // Convert the cloud events into domain events
-        val domainEventsInStream: Stream<DomainEvent> = eventStream.events().map(cloudEventConverter::toDomainEvent)
+        val domainEventsInStream: Sequence<DomainEvent> = eventStream.events().map(cloudEventConverter::toDomainEvent).asSequence()
 
-        // Call a pure function from the domain model which returns a Stream of domain events
+        // Call a pure function from the domain model which returns a sequence of domain events
         val newDomainEvents = functionThatCallsDomainModel(domainEventsInStream)
 
         // Convert domain events to cloud events and write them to the event store
-        eventStore.write(streamId, eventStream.version(), newDomainEvents.map(cloudEventConverter::toCloudEvent))
+        eventStore.write(streamId, eventStream.version(), newDomainEvents.map(cloudEventConverter::toCloudEvent).asStream())
     }
 }
