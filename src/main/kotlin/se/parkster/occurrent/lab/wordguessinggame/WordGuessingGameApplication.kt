e0d7ee895bbd8395cb1ca3baa6f11df84c51a168
Johan Haleby <johan.haleby@gmail.com>
2020-11-01 08:11:36 +0100

    Upgraded to Occurrent 0.2.0

--- src/main/kotlin/se/parkster/occurrent/lab/wordguessinggame/WordGuessingGameApplication.kt
+++ src/main/kotlin/se/parkster/occurrent/lab/wordguessinggame/WordGuessingGameApplication.kt
@@ -1,10 +1,27 @@
 package se.parkster.occurrent.lab.wordguessinggame
 
+import org.occurrent.application.converter.CloudEventConverter
+import org.occurrent.application.service.blocking.ApplicationService
+import org.occurrent.application.service.blocking.implementation.GenericApplicationService
+import org.occurrent.eventstore.api.blocking.EventStore
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import se.parkster.occurrent.lab.wordguessinggame.domain.DomainEvent
 
 @SpringBootApplication
-class WordGuessingGameApplication
+class WordGuessingGameApplication {
+
+    @Bean
+    fun cloudEventConverter(): CloudEventConverter<DomainEvent> = TODO()
+
+    @Bean
+    fun eventStore(): EventStore = TODO()
+
+    @Bean
+    fun applicationService(eventStore: EventStore, cloudEventConverter: CloudEventConverter<DomainEvent>): ApplicationService<DomainEvent> =
+            GenericApplicationService(eventStore, cloudEventConverter)
+}
 
 fun main(args: Array<String>) {
     runApplication<WordGuessingGameApplication>(*args)
